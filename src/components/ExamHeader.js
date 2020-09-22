84de9b6d8d74341f84fcc0afea346545001b6e6b
danielm <dm1905once@gmail.com>
2020-09-21 17:12:35 -0700

    Updated Header component

--- src/components/ExamHeader.js
+++ src/components/ExamHeader.js
@@ -3,23 +3,18 @@ import React from 'react';
 
 const ExamHeader = () => {
     return (
-        <div className="ui centered grid">
-
-            <div className="fluid row">
-                <div className="eight wide column">
-                    
-                        <h2 className="ui header">
-                            <i class="file alternate blue icon"></i>
-                            <div class="content">Exam name here</div>
-                        </h2>
-                </div>
-                <div className="six wide column">
-                        <div class="ui  basic  label">
-                            Owner
-                        </div>
-                </div>
+        <div class="ui raised horizontal segments">
+            <div class="ui segment">
+                <h2 className="ui header">
+                    <i class="file alternate blue icon"></i>
+                    <div class="content">Exam name here</div>
+                </h2>
+            </div>
+            <div class="ui right aligned segment">
+                <p>Owner</p>
             </div>
         </div>
+
     )
 }
 
