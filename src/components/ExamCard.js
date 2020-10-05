cc8604ca008696d127d92d1b0a81b976db675f5b
danielm <dm1905once@gmail.com>
2020-10-04 22:49:00 -0700

    Start building the ExamCard component

---
+++ src/components/ExamCard.js
@@ -0,0 +1,76 @@
+import React from 'react';
+
+const ExamCard = () =>{
+    return (
+        <div className="card">
+            <div className="content">
+                <img className="right floated mini ui image" src="/images/avatar/large/elliot.jpg"/>
+                <div className="header">
+                    Elliot Fu
+                </div>
+                <div className="meta">
+                    Friends of Veronika
+                </div>
+                <div className="description">
+                    Elliot requested permission to view your contact details
+                </div>
+                <table className="ui small table">
+                    <thead>
+                        <tr>
+                        <th>Passing Score</th>
+                        <th>Fee</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>70</td>
+                        <td>80.99</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+
+
+{/* 
+                <div className="ui divided list">
+                    <div className="item">
+                        <i className="dollar sign icon"></i>
+                        <div className="content">
+                        <div className="header">Exam fee: </div>99.00
+                        </div>
+                    </div>
+                    <div className="item">
+                        <i className="percent icon"></i>
+                        <div className="content">
+                        <div className="header">Passing Score: </div>70
+                        </div>
+                    </div>
+                </div>
+ */}
+
+
+{/* 
+            <div className="ui horizontal statistics">
+                <div className="ui violet statistic">
+                    <div className="label">Passing Score</div>
+                    <div className="value">70</div>
+                </div>
+                <div className="ui violet statistic">
+                    <div className="label">Fee</div>
+                    <div className="value"><i className="dollar sign icon"></i>80.99</div>
+                </div>
+            </div>
+                 */}
+
+            </div>
+            <div className="extra content">
+            <div className="ui two buttons">
+                <div className="ui basic green button"><i className="edit alternate icon"></i>Edit</div>
+                <div className="ui basic red button"><i className="trash alternate icon"></i>Delete</div>
+            </div>
+            </div>
+        </div>
+    )
+};
+
+export default ExamCard;
--- src/components/ExamList.js
+++ src/components/ExamList.js
@@ -1,16 +1,35 @@
 import React from 'react';
-import ExamCreate from './ExamCreate';
 import { useLocation  } from 'react-router-dom';
+import ExamCreate from './ExamCreate';
+import ExamCard from './ExamCard';
 
 const ExamList = () => {
     const location = useLocation();
-    const topMessage = location.state? location.state.topMessage : "Welcome!";
+    const topMessage = location.state? location.state.topMessage : null;
 
     return (
         <div className="ui container">
             {topMessage?<div className={`ui  message`}>{topMessage}</div>:''}
+
+            <h2 className="ui block header">
+                <i className="file outline icon"></i>
+                <div className="content">Create a new Exam</div>
+            </h2>
             <ExamCreate />
-            {/* Add individual exam cards here */}
+
+            <h2 className="ui block header">
+                <i className="copy outline icon"></i>
+                <div className="content">Your exams</div>
+            </h2>
+            <div class="ui cards">
+            <ExamCard />
+            <ExamCard />
+            <ExamCard />
+            <ExamCard />
+            <ExamCard />
+            <ExamCard />
+            <ExamCard />
+            </div>
         </div>
     )
 }
