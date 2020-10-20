91cc72e56398b471e80dc6a54151f34675bb38d4
danielm <dm1905once@gmail.com>
2020-10-19 17:24:13 -0700

    Started to add choices

--- src/components/applicants/ApplyExam.js
+++ src/components/applicants/ApplyExam.js
@@ -13,7 +13,6 @@ const ApplyExam = () =>{
     const [ examInStore, setExamInStore ] = useState(false);
     const [ currentQuestion, setCurrentQuestion ] = useState(1);
     const activeExam = useSelector(store=> store.activeExam);
-    // const [ validExamId, setValidExamId ] = useState(null);
 
     useEffect(()=>{
         async function validateAndGetExam(){
@@ -47,7 +46,7 @@ const ApplyExam = () =>{
         <div>
             {(isApplicantAuth && isValidExam) ? (
                 <div>
-                    <h1>Question navigator</h1>
+                    <h1>{activeExam.exam_name}</h1>
                     <ShowCrumbs 
                         examReady={examInStore} 
                         currentQuestion={currentQuestion} 
@@ -67,19 +66,52 @@ const ApplyExam = () =>{
 
 function ShowQuestion( {examReady, currentQuestion, activeExam, handlePrev, handleNext} ){
     if (examReady){
+
+        function handleSelectedChoice(e){
+            console.log(e.target.value);
+        }
     return (
         <div>
-            <p>Showing questions here. Total # questions: {activeExam.questions.length} Current question is: {currentQuestion}</p>
-            <button 
-                onClick={handlePrev}
-                className={'ui labeled icon gray button ' + (currentQuestion===1?'disabled':'')}>
-                <i className="left arrow icon"></i>Previous
-            </button>
-            <button 
-                onClick={handleNext}
-                className={'ui right labeled icon blue button ' + (currentQuestion===activeExam.questions.length?'disabled':'')}>
-                <i className="right arrow icon"></i>Next
-            </button>
+            <h3>Select the choice that best answers the question:</h3>
+            <div className="ui blue raised padded segment">{activeExam.questions[currentQuestion-1].question_text}</div>
+            <div className="ui grid">
+                <div className="ui row">
+                    <div className="ui twelve wide column">
+                        <div className="ui form" onChange={handleSelectedChoice}>
+                            <div className="grouped fields">
+                            <label htmlFor="fruit">Select one choice:</label>
+                                <div className="field">
+                                    <div className="ui radio checkbox">
+                                        <input type="radio" name="fruit" tabIndex="0" value="algo"/>
+                                        <label>Apples</label>
+                                    </div>
+                                </div>
+                                <div className="field">
+                                    <div className="ui radio checkbox">
+                                        <input type="radio" name="fruit" tabIndex="0" value="nada" />
+                                        <label>Oranges</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="ui four wide column">
+                    <button 
+                        onClick={handlePrev}
+                        className={'ui labeled icon gray button ' + (currentQuestion===1?'disabled':'')}>
+                        <i className="left arrow icon"></i>Previous
+                    </button>
+                    <button 
+                        onClick={handleNext}
+                        className={'ui right labeled icon blue button ' + (currentQuestion===activeExam.questions.length?'disabled':'')}>
+                        <i className="right arrow icon"></i>Next
+                    </button>
+                    </div>
+                </div>
+                <div className="ui row">
+                    <button className="fluid ui button">Finish and Submit Exam</button>
+                </div>
+            </div>
         </div>
     )
     } else {
@@ -90,7 +122,7 @@ function ShowQuestion( {examReady, currentQuestion, activeExam, handlePrev, hand
 function ShowCrumbs( {examReady, currentQuestion, activeExam} ){
     if (examReady){
         return (
-            <div className="ui raised segment">
+            <div className="ui secondary segment">
                 {activeExam.questions.map((q,i)=>
                     <div 
                         className={`ui ${(parseInt(currentQuestion) === i+1)? 'green': 'gray'} circular label`}
